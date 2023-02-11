9e5c1dbf2b26042be46e7d7311da03285a4e83db
Tausif Qureshi <59664677+Tausifqureshi786@users.noreply.github.com>
2023-02-10 18:53:43 -0700

    Full completed project along with the completed challeges from 100 days of swift ui

---
+++ WordScramble /WordScramble/WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by Tausif Qureshi on 2023-02-06.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
